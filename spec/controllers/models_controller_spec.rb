2984bc0f76448b38a7a830263cd9804670be72c2
Molly O'Neil <mboneil@umass.edu>
2016-09-14 10:20:59 -0400

    Editing models controller to accept JSON request

--- spec/controllers/models_controller_spec.rb
+++ spec/controllers/models_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe ModelsController do
   describe 'GET #index' do
-    it 'is not empty' do
-      get :index
-      expect(response).not_to be_empty
+    it 'returns a nonempty JSON' do
+      get :index, format: :json
+      expect(response.status).to be (204)
     end
   end
 end
