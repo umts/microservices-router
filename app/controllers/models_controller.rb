4282f24fd018b11bc17ee84e03107921a5176111
Molly O'Neil <mboneil@umass.edu>
2016-09-15 13:34:36 -0400

    Updating models to include only name for JSON

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -2,6 +2,6 @@ class ModelsController < ApplicationController
   def index
     render json: Service.all, 
     except: %i(created_at updated_at id), 
-    include: { models: { only: [:service, :name, :url] }}
+    include: { models: { only: :name }}
   end
 end
