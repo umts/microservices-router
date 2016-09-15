40e422048814fe3ffb4a0ec5084e077edc01288b
David Faulkenberry <david.faulkenberry@gmail.com>
2016-09-15 17:58:47 -0400

    Use API documentation DSL

--- spec/controllers/models_controller_spec.rb
+++ spec/controllers/models_controller_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
-require 'rspec_api_documentation/dsl'
 
-describe ModelsController do
-  describe 'GET #index' do
+resource 'Models' do
+  get '/models' do
     let(:service_1) { create :service }
     let(:service_2) { create :service }
     let!(:model_1){ create :model, service: service_1 }
     let!(:model_2){ create :model, service: service_1 }
     let!(:model_3){ create :model, service: service_2 }
-    it 'renders JSON with services and their models' do
-      get :index, format: :json
-      body = JSON.parse response.body
+    example 'Listing all models by service' do
+      do_request
+      body = JSON.parse response_body
       body.each(&:deep_symbolize_keys!)
       expect(body).to eql [
         { url: service_1.url,
