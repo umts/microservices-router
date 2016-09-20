204f6fff037117e1c422ddb6fc4d95e1a72d2a62
Molly O'Neil <mboneil@umass.edu>
2016-09-20 11:09:46 -0400

    Adding test about service existing and all models registered

--- spec/acceptance/services_controller_spec.rb
+++ spec/acceptance/services_controller_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 resource 'Services' do
   post '/services/register' do
+    parameter :service, @service_1
     example 'Creating and returning a nested data structure' do
       expect{do_request}
         .to change{Service.count}
         .by 1
       body = JSON.parse response_body
       expect(body).not_to be_empty
+      #Need to verify returned is a nested data structure
+    end
+    example 'Returns empty if service already exists and has all models registered' do
+      @service_1 = create :service
+      @model_1 = create :model, service: @service_1
+      @model_2 = create :model, service: @service_1
+      do_request
+      body = JSON.parse response_body
+      expect(body).to be_empty
+    end
+    example 'Returns empty with model name collision' do
     end
   end
 end
