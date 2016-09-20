44af3a8a59054766a13c4497569591718cfc7711
Molly O'Neil <mboneil@umass.edu>
2016-09-20 10:13:32 -0400

    Indentation on unit test

--- spec/acceptance/services_controller_spec.rb
+++ spec/acceptance/services_controller_spec.rb
@@ -4,8 +4,8 @@ resource 'Services' do
   post '/services/register' do
     example 'Creating and returning a nested data structure' do
       expect{do_request}
-      .to change{Service.count}
-      .by 1
+        .to change{Service.count}
+        .by 1
       body = JSON.parse response_body
       expect(body).not_to be_empty
     end
