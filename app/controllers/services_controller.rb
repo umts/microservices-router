73b46f71f7e1e3ae83d3b59dd75a522f25229fe5
Molly O'Neil <mboneil@umass.edu>
2016-09-20 13:42:15 -0400

    Editing the tests and services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,12 @@
 class ServicesController < ApplicationController
   def register
-    @service = Service.create
-    render json: @service, include: :models
+    @service = Service.create(params[:id])
+    if @service.save
+      render json: @service, 
+            except: %i(created_at updated_at id),
+            include: :models  
+    else
+      render json: {status: 'Service already exists and has all models registered'}
+    end
   end
 end
--- spec/acceptance/services_controller_spec.rb
+++ spec/acceptance/services_controller_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 resource 'Services' do
   post '/services/register' do
     let(:service_1) { create :service }
+    let(:service_2) { create :service }
     let(:model_1) { create :model, service: service_1 }
     let(:model_2) { create :model, service: service_1 }
+    let(:model_3) { create :model }
     example 'Creating and returning a nested data structure' do
       data = {url: service_1.url, models:[{name: model_1.name}, {name: model_2.name}]}
       expect{do_request(service: data)}
@@ -12,11 +14,18 @@ resource 'Services' do
         .by 1
       body = JSON.parse response_body
       body.deep_symbolize_keys!
-      expect(body).to eql data 
+      expect(body).not_to be_empty
+      #need to check for nested data structure
     end
     example 'Returns empty if service already exists and has all models registered' do
+      do_request 
+      body = JSON.parse response_body
+      expect(body).to be_empty
     end
-    example 'Returns empty with model name collision' do
+    example 'Returns unprocessable entity with model name collision' do
+      do_request
+      body = JSON.parse response_body
+      expect(body).to eql ({status: 'Service already exists and has all models registered'})
     end
   end
 end
