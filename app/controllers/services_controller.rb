28dba5477f0497bceaa951299af28e0eae9c7e14
Molly O'Neil <mboneil@umass.edu>
2016-09-21 13:00:10 -0400

    Creating a service according to the given parameters

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,12 +1,11 @@
 class ServicesController < ApplicationController
   def register
-    @service = Service.create(params[:id])
-    if @service.save
-      render json: @service, 
-            except: %i(created_at updated_at id),
-            include: :models  
-    else
-      render json: {status: 'Service already exists and has all models registered'}
-    end
+    @service = Service.create
+    @service.url = params[:url]
+    @models = params[:models]
+    @models.each{ |m| Model.create(service: @service)}
+    render json: @service, 
+              except: %i(created_at updated_at id),
+              include: :models
   end
 end
--- spec/acceptance/services_controller_spec.rb
+++ spec/acceptance/services_controller_spec.rb
@@ -2,30 +2,20 @@ require 'rails_helper'
 
 resource 'Services' do
   post '/services/register' do
-    let(:service_1) { create :service }
-    let(:service_2) { create :service }
+    let(:service_1) { create :service, url: 'hotcakes-now.com' }
+    let(:service_2) { create :service, url: 'massivebus.com'}
     let(:model_1) { create :model, service: service_1 }
     let(:model_2) { create :model, service: service_1 }
     let(:model_3) { create :model }
     example 'Creating and returning a nested data structure' do
       data = {url: service_1.url, models:[{name: model_1.name}, {name: model_2.name}]}
-      expect{do_request(service: data)}
+      expect{do_request(data)}
         .to change{Service.count}
         .by 1
       body = JSON.parse response_body
       body.deep_symbolize_keys!
       expect(body).not_to be_empty
       #need to check for nested data structure
-    end
-    example 'Returns empty if service already exists and has all models registered' do
-      do_request 
-      body = JSON.parse response_body
-      expect(body).to be_empty
-    end
-    example 'Returns unprocessable entity with model name collision' do
-      do_request
-      body = JSON.parse response_body
-      expect(body).to eql ({status: 'Service already exists and has all models registered'})
     end
   end
 end
