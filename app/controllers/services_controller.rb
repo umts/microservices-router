504d425a9b81e2dfa160ea81ad5ee12aafa9647c
Molly O'Neil <mboneil@umass.edu>
2016-09-23 14:41:09 -0400

    Updating the services controller and test to behave correctly

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,11 +1,15 @@
 class ServicesController < ApplicationController
   def register
-    @service = Service.create(url: params[:url])
-    if params[:models].present?
-      @models = params[:models]
-      @models.each{ |m| Model.create(service: @service)}
+    if Service.find_by(url: params[:url])
+      render nothing: true, status: :no_content
+    else
+      @service = Service.create(url: params[:url])
+      if params[:models].present?
+        @models = params[:models]
+        @models.each{ |m| Model.create(service: @service)}
+      end
+      render json: {url: @service.url, models: @models},
+                except: %i(created_at updated_at id) 
     end
-    render json: {url: @service.url, models: @models},
-              except: %i(created_at updated_at id)        
   end
 end
--- spec/acceptance/services_controller_spec.rb
+++ spec/acceptance/services_controller_spec.rb
@@ -5,14 +5,11 @@ resource 'Services' do
     let(:service_1) { create :service }
     let(:model_1) { create :model, service: service_1 }
     let(:model_2) { create :model, service: service_1 }
-    example 'Creating and returning a nested data structure' do
-      service_data = {url: service_1.url, models:[{name: model_1.name}, {name: model_2.name}]}
-      expect{do_request(service_data)}
-        .to change{Service.count}
-        .by 1
-      body = JSON.parse response_body
-      body.deep_symbolize_keys!
-      expect(body).to eql service_data
+    example 'Doing nothing on already registered services and models' do
+      service_data = { url: service_1.url, models:[{name: model_1.name}, {name: model_2.name}] }
+      expect{ do_request(service_data) }
+        .not_to change{ Service.count }
+      expect(status).to be status_code(:no_content)
     end
   end
 end
