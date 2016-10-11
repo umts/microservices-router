9e155bf9dfb3f0d3338bc255e01c9af997b4d4c1
Molly O'Neil <mboneil@umass.edu>
2016-10-11 12:26:44 -0400

    Indenting services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,18 +1,18 @@
 class ServicesController < ApplicationController
   include ServiceChangeNotifier
-  def register
-    service = Service.find_or_create_by url: params.require(:url)
-    params.require(:models).each do |model_data|
-      model = Model.find_by name: model_data.require(:name)
-      if model.present?
-        head :unprocessable_entity and return if model.service != service
-      else
-        Model.create(name: model_data.require(:name), service: service)
+    def register
+      service = Service.find_or_create_by url: params.require(:url)
+      params.require(:models).each do |model_data|
+        model = Model.find_by name: model_data.require(:name)
+        if model.present?
+          head :unprocessable_entity and return if model.service != service
+        else
+          Model.create(name: model_data.require(:name), service: service)
+        end
       end
-    end
-    render json: service,
-           only: :url,
-           include: { models: { only: :name } }
-    ServiceChangeNotifier.notify_services_of_changes if service.changed?
+      render json: service,
+             only: :url,
+             include: { models: { only: :name } }
+      ServiceChangeNotifier.notify_services_of_changes if service.changed?
   end
 end
