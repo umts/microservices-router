ad63dcb229b41a6440f98a2231daa709d21eea66
Molly O'Neil <mboneil@umass.edu>
2016-10-11 12:56:51 -0400

    Rewriting 'post' to 'post_form' and expanding if statement for services notification

--- lib/microservices_router.rb
+++ lib/microservices_router.rb
@@ -3,9 +3,9 @@ require 'net/http'
 module ServiceChangeNotifier
   def self.notify_services_of_changes
     Service.all.each do |s|
-      request = Net::HTTP.post(URI(s.url), Service.all.to_json(include: :models))
+      request = Net::HTTP.post_form(URI(s.url), Service.all.to_json(include: :models))
       if request.nil?
-        Net::HTTP.post(URI(s.url), 'an error occurred with the request')
+        Net::HTTP.post_form(URI(s.url), 'an error occurred with the request')
       end
     end
   end
