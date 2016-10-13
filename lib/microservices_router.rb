f42ad439d1afc666c95c64bccbcb3d660b031309
Molly O'Neil <mboneil@umass.edu>
2016-10-13 12:16:03 -0400

    Correctly writing net http posts and fixing urls in factory

--- lib/microservices_router.rb
+++ lib/microservices_router.rb
@@ -3,9 +3,11 @@ require 'net/http'
 module ServiceChangeNotifier
   def self.notify_services_of_changes
     Service.all.each do |s|
-      request = Net::HTTP.post_form(URI(s.url), Service.all.to_json(include: :models))
-      if request.nil?
-        Net::HTTP.post_form(URI(s.url), 'an error occurred with the request')
+      unless s.url.nil?
+        request = Net::HTTP.post_form(URI(s.url), {'services' => Service.all.to_json(include: :models)} )
+        if request.nil?
+          Net::HTTP.post_form(URI(s.url), {'error' => 'an error occurred with the request'})
+        end
       end
     end
   end
