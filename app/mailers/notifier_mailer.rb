fd209735458cfe52200b54611db265f642436f06
Molly O'Neil <mboneil@umass.edu>
2016-10-18 12:23:56 -0400

    Creating mail for service change error

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,2 +1,7 @@
 class NotifierMailer < ApplicationMailer
+  def service_change_error(response_code)
+    mail(from: 'transit-it@admin.umass.edu',
+         to: 'programmers@admin.umass.edu',
+         subject: "Service Change Notifier has a #{response_code} error")
+  end
 end
