1d0667900ae9b00d7d9d434409a82f3e0ef739bf
Molly O'Neil <mboneil@umass.edu>
2016-10-20 12:14:14 -0400

    Running rubocop

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,9 +1,9 @@
 class NotifierMailer < ApplicationMailer
-
   def service_change_error(response_code)
-    #want to replace with CONFIG.fetch(:notification_email)
+    # want to replace with CONFIG.fetch(:notification_email)
     mail to: 'transit-it@admin.umass.edu',
-          subject: "Service Change Notifier has failed",
-          body: "The service change notifier request failed with the following status code: #{response_code}."
+         subject: 'Service Change Notifier has failed',
+         body: "The service change notifier request failed
+         with the following status code: #{response_code}."
   end
 end
--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 resource 'NotifierMailer' do
-    example 'Returning a nested data structure for registered service' do
-      email = NotifierMailer.service_change_error('404')
-      expect(email.body).to include 'status code: 404.'
-    end
+  example 'Returning a nested data structure for registered service' do
+    email = NotifierMailer.service_change_error('404')
+    expect(email.body).to include 'status code: 404.'
+  end
 end
