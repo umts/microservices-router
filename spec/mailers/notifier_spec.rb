291d313c7905bb22fe7bef2134fb49fe9a14d9d2
Molly O'Neil <mboneil@umass.edu>
2016-10-20 12:07:06 -0400

    Testing email body

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 resource 'NotifierMailer' do
     example 'Returning a nested data structure for registered service' do
       email = NotifierMailer.service_change_error('404')
-      expect(email.body).to include '404 error'
+      expect(email.body).to include 'status code: 404.'
     end
 end
