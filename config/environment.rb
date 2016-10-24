f3f3eeef2b54c80fd79c907493face0308587b71
Molly O'Neil <mboneil@umass.edu>
2016-10-24 11:19:23 -0400

    Making config constant into one line

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-CONFIG = YAML.load_file('config/application.yml')
-CONFIG.symbolize_keys!
+CONFIG = YAML.load_file('config/application.yml').symbolize_keys
