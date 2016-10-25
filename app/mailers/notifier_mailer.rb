9a1a93a79ac9cf10517ce629fbb1ca9220519f6e
Molly O'Neil <mboneil@umass.edu>
2016-10-25 10:00:38 -0400

    Removing single quotes and variable declaration

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,7 +1,6 @@
 class NotifierMailer < ActionMailer::Base
   def service_change_error(response_code)
-    notification_email = CONFIG.fetch(:notification_email)
-    mail to: notification_email,
+    mail to: CONFIG.fetch(:notification_email),
          subject: 'Service Change Notifier has failed',
          body: "The service change notifier request failed
          with the following status code: #{response_code}."
