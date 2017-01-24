776db0d54b091ed4a7f23d3bbad388c086e22c46
Molly O'Neil <mboneil@umass.edu>
2017-01-24 13:41:21 -0500

    Removing newline in email body

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -2,7 +2,6 @@ class NotifierMailer < ActionMailer::Base
   def send_mail(service, response_code)
     mail to: CONFIG.fetch(:notification_email),
          subject: 'Service Change Notifier has failed',
-         body: "The service change notifier request failed
-         with the service #{service} and status code #{response_code}."
+         body: "The service change notifier request failed with the service #{service} and status code #{response_code}."
   end
 end
