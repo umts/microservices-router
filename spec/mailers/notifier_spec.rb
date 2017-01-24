b684063400cd2444326f42674da439551472fce5
Molly O'Neil <mboneil@umass.edu>
2017-01-24 13:38:55 -0500

    Update specs to account for service mentioned in mailer

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 resource 'NotifierMailer' do
   example 'Returning a nested data structure for registered service' do
-    email = NotifierMailer.send_mail('404')
-    expect(email.body).to include 'status code: 404.'
+    email = NotifierMailer.send_mail('www.example.com', '404')
+    expect(email.body).to include 'status code 404.'
+    expect(email.body).to include 'service www.example.com'
   end
 end
