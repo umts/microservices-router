dd90df877321f759dc7f85c2488b430ce56e199c
Molly O'Neil <mboneil@umass.edu>
2017-01-24 13:29:09 -0500

    Update mailer method to mention problematic service

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -1,8 +1,8 @@
 class NotifierMailer < ActionMailer::Base
-  def send_mail(response_code)
+  def send_mail(service, response_code)
     mail to: CONFIG.fetch(:notification_email),
          subject: 'Service Change Notifier has failed',
          body: "The service change notifier request failed
-         with the following status code: #{response_code}."
+         with the service #{service} and status code #{response_code}."
   end
 end
