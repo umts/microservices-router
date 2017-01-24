f1af7540a64a37e6cd38122f532b9bbecf578992
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-01-24 15:49:32 -0500

    remove nil check from notifier, begin work on spec. Not even close yet

--- app/lib/service_change_notifier.rb
+++ app/lib/service_change_notifier.rb
@@ -2,18 +2,19 @@ require 'net/http'
 
 module ServiceChangeNotifier
   def notify_services_of_changes (initiating_service)
-    Service.all.each do |s|
-      next if (s.url.nil? or s.id == initiating_service.id)
+    Service.each do |s|
+      next if (s.id == initiating_service.id)
       request = Net::HTTP.post_form(URI(s.url),
                                     'services' =>
                                       Service.all.to_json(include: :models))
       if request.nil?
         Net::HTTP.post_form(URI(s.url),
                             'error' => 'Request is nil')
-      elsif request == Net::HTTPSuccess
       else
-        send_mail(status)
-        request.retry
+        if request != Net::HTTPSuccess
+          send_mail(status)
+          request.retry
+        end
       end
     end
   end
--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -6,3 +6,26 @@ resource 'NotifierMailer' do
     expect(email.body).to include 'status code: 404.'
   end
 end
+
+resource 'ServiceChangeNotifier' do
+  # include ServiceChangeNotifier
+  post '/services/register' do
+    let(:service_1) { create :service }
+    let(:model_2) { create :model, service: service_1 }
+    parameter :url, 'Unique identifier for services', required: true
+    parameter :models, 'Models assigned to a specific service', required: false
+
+    example 'Services are notified when a change is triggered' do
+      let(:model_1) { create :model, service: service_1 }
+      service_data = { url: 'https://www.example.com/abc', models: 'amazing_model' }
+      expect(Net::HTTP).to receive(:post_form).with(URI(service_1.url), services: {  }) 
+      do_request(service_data)
+    end
+    # example 'Services without URLs are not notified of service changes' do
+    #   service_no_url = create :service, url: nil
+    #   service_data = { url: 'https://www.example.com/abc', models: 'amazing_model' }
+    #   expect(service_1).not_to receive :notify_services_of_changes
+    #   do_request(service_data)
+    # end
+  end
+end
