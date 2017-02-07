c28dff8bc502e524cda8a022e3652286d97bfa82
Aaron Kaplowitz <akaplowitz@umass.edu>
2017-02-07 15:31:50 -0500

    Compare objects instead of IDs

--- app/lib/service_change_notifier.rb
+++ app/lib/service_change_notifier.rb
@@ -3,7 +3,7 @@ require 'net/http'
 module ServiceChangeNotifier
   def notify_services_of_changes(initiating_service)
     Service.all.each do |s|
-      next if s.id == initiating_service.id
+      next if s == initiating_service
       response = Net::HTTP.post_form(URI(s.url),
                                      'services' =>
                                      Service.all.to_json(only: :url,
